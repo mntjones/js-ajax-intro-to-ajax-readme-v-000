a286b9537d7dd98adb27e3d19d4283f4cd029ad0
Monica Tamaru Jones <mntamaru@gmail.com>
2018-08-17 23:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function showRepositories(event, data) {
-  console.log(this.responseText)
-  let repoList = "<ul>"
+  console.log(this.responseText);
+  let repoList = "<ul>";
+  
   for(var i=0;i < this.responseText.length; i++) {
-    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>";
   }
-  repoList += "</ul>"
-  document.getElementById("repositories").innerHTML = repoList
+  repoList += "</ul>";
+  document.getElementById("repositories").innerHTML = repoList;
 }
 
 function getRepositories() {
