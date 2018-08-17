f0b2c19ccd11fef62341aae3b91483e2397a38ef
Monica Tamaru Jones <mntamaru@gmail.com>
2018-08-17 23:44:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener("load", showCommits);
-  req.open("GET", 'http://api.github.com/repos/octocat')
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
 }
